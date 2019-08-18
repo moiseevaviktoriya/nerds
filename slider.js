a687ddae767c2784f84cd723f457f7d52e29a35f
moiseevaviktoriya <moiseevaviktoriya@gmail.com>
2019-08-18 18:39:16 +0300

    changed styles. add slider swipe

--- slider.js
+++ slider.js
@@ -17,25 +17,80 @@ let sliders = [{
         'src': './slider/nerds-index-slide3.png'
     }];
 
-let indicators = document.querySelector('.slider__indicators');
-indicators.addEventListener('click', changeSlide);
-
-function changeSlide(e) {
-    
-    let indicator = document.querySelectorAll('.indicator'),
-        indicatorArr = Array.from(indicator),
-        slider = document.querySelector('.slider__img'),
+let indicatorsList = document.querySelector('.slider__indicators');
+let slider = document.querySelector('.slider__img'),
         sliderTitle = document.querySelector('.slider__title'),
         sliderDescription = document.querySelector('.slider__description');
-    
+
+if (sliders.length !== 0) {
+        setSliderContent(0);
+    for (let i = 0; i < sliders.length; i++) {
+        let indicator = document.createElement('li');
+        i === 0? indicator.classList.add('indicator', 'active__indicator') : indicator.classList.add('indicator');
+        indicatorsList.append(indicator);
+    }
+} else {
+    document.querySelector('.slider').style.display = 'none';
+}
+
+let indicators = document.getElementsByClassName('slider__indicators')[0];
+
+indicators.addEventListener('click', changeSlide);
+
+let indicator = document.getElementsByClassName('indicator'),
+        indicatorArr = Array.from(indicator);
+
+function changeSlide(e) {
     indicatorArr.forEach(function (item, i) {
         if (item === e.target && !item.classList.contains('active__indicator')) {
           item.classList.add('active__indicator');
-          slider.src = sliders[i].src;
-          sliderTitle.textContent = sliders[i].title;
-          sliderDescription.textContent = sliders[i].description;
-        } else if (item !== e.target) {
+          setSliderContent(i);
+        } else if (item !== e.target && e.target !== indicators) {
           item.classList.remove('active__indicator');  
         }
     })
+}
+
+function setSliderContent(pos) {
+    slider.src = sliders[pos].src;
+    sliderTitle.textContent = sliders[pos].title;
+    sliderDescription.textContent = sliders[pos].description;
+}
+
+let initialPoint,
+    finalPoint,
+    sliderToSwipe = document.querySelector('.slider__col:nth-child(2)'),
+    currentSlide = document.getElementsByClassName('active__indicator')[0];
+
+sliderToSwipe.addEventListener('touchstart', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    initialPoint = event.changedTouches[0];
+}, false);
+
+sliderToSwipe.addEventListener('touchend', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    finalPoint = event.changedTouches[0];
+    let xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+    
+    if (xAbs > 20) {
+        if (finalPoint.pageX > initialPoint.pageX) {
+            if (currentSlide.previousSibling) {
+                changeSlideBySwipe(-1, currentSlide.previousSibling);
+            }
+        } else {
+            if (currentSlide.nextSibling) {
+                changeSlideBySwipe(1, currentSlide.nextSibling);
+            }
+        }
+    }
+}, false);
+
+function changeSlideBySwipe(num, sibling) {
+    currentSlide.classList.remove('active__indicator');
+    sibling.classList.add('active__indicator');
+    let currentIndex = indicatorArr.indexOf(currentSlide);
+    currentSlide = sibling;
+    setSliderContent(currentIndex + num);
 }
