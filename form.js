fbe4b55360b74e9c5d4dcc463b84d819eaf10050
moiseevaviktoriya <moiseevaviktoriya@gmail.com>
2019-08-19 22:45:13 +0300

    map section, about us section, partners

--- form.js
+++ form.js
@@ -4,11 +4,25 @@ let showFormBtn = document.querySelector('.address__col__button'),
     form = document.querySelector('.form__container'),
     closeFormBtn = document.querySelector('.form__close-icon'),
     sendFormBtn = document.querySelector('.form__col-button'),
-    formFields = document.querySelectorAll('.form-control');
+    formFields = document.querySelectorAll('.form-control'),
+    addressCol = document.querySelector('.address__col'),
+    addressColOpen = document.querySelector('.address__col-open'),
+    addressColClose = document.querySelector('.address__col-close-icon');
 
+if (window.innerWidth > '768') {
+    addressCol.classList.add('address__col-active');
+}
+
+addressColOpen.addEventListener('click', addressColToggle);
+addressColClose.addEventListener('click', addressColToggle);
 showFormBtn.addEventListener('click', toggleForm);
 closeFormBtn.addEventListener('click', toggleForm);
 sendFormBtn.addEventListener('click', sendForm);
+
+function addressColToggle(event) {
+    event.preventDefault();
+    addressCol.classList.toggle('address__col-active');
+}
 
 function toggleForm(event) {
     event.preventDefault();
@@ -22,3 +36,4 @@ function sendForm(event) {
         form.classList.remove('active-form');
     }
 }
+
